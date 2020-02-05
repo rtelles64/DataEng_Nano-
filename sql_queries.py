2dfcac47bad3aa679b76b0023582503652c56a42
rtelles64 <rtelles64@gmail.com>
2020-02-04 17:12:57 -0800

    Add artists and time table inserts

--- sql_queries.py
+++ sql_queries.py
@@ -52,10 +52,14 @@ song_table_insert = ("""
 """)
 
 artist_table_insert = ("""
+    INSERT INTO artists (artist_id, name, location, latitude, longitude)
+    VALUES (%s, %s, %s, %s, %s);
 """)
 
 
 time_table_insert = ("""
+    INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+    VALUES (%s, %s, %s, %s, %s, %s, %s);
 """)
 
 # FIND SONGS
