49025e3af532ad3e490653236db00cde3d467f12
rtelles64 <rtelles64@gmail.com>
2020-02-04 22:40:39 -0800

    Implement all queries

--- sql_queries.py
+++ sql_queries.py
@@ -71,6 +71,10 @@ time_table_insert = ("""
 # FIND SONGS
 
 song_select = ("""
+    SELECT s.song_id, a.artist_id
+    FROM songs s
+    JOIN artists a ON s.artist_id = a.artist_id
+    WHERE s.title = %s AND a.name = %s AND s.duration = %s;
 """)
 
 # QUERY LISTS
